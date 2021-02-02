c3f8f47a6d4de43860068cf5bf56a7cea0eb53c3
harikrishna2208 <harikrishna2208>
2021-02-03 01:23:12 +0530

    Partital Working

--- App.js
+++ App.js
@@ -1,39 +1,33 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import Quotes from "./quotesAndJs/Quotes";
+
 
 export default function App() {
   return (
     <View>
-      
       <View style={styles.container}>
-         <Text style={styles.Title}>Quotes Apps</Text>
+        <Text style={styles.Title}>Quotes Apps</Text>
         <View style={styles.Quote}>
-          <Text>Good its fadiashhj;osjarfhoahf;oRunning</Text>
-           <Text>oaydogaiduifpauapwdpaifigapdfwafap</Text>
-           <Text>oaydogaiduifpauapwdpaifigapdfwafap</Text>
+          <Quotes />
         </View>
       </View>
       <View style={styles.buttonarea}>
-        <Button title="Next Quote"  />
+        <Button title="Next Quote" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  Title:{
-   
-    margin:10,
-    fontSize:20,    
-    textAlign:"center",
+  Title: {
+    margin: 10,
+    fontSize: 20,
+    textAlign: "center",
   },
   container: {
     marginTop: 150,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-    // borderWidth: 3,
-    // borderColor: "#dc143c",
     marginBottom: 10,
   },
   Quote: {
@@ -46,7 +40,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   buttonarea: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
